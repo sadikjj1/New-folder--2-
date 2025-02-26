a101bf5c352db35e31633947dfaf2c870fa1e729
sadikjj1 <islam15-5550@diu.edu.bd>
2025-02-26 10:59:56 +0600

    new

--- learnly/lib/main.dart
+++ learnly/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,35 @@ class AdvanceUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Advance UI"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              ListTile(
+                leading: Icon(Icons.lock_clock_outlined),
+                title: Text("recent"),
+              )
+            ],
+          ),
+          
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              CachedNetworkImage(
+                imageUrl: "http://via.placeholder.com/350x150",
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
